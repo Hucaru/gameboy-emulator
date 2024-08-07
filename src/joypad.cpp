f78200afac39ace1c15d2b72b707c36558fb271e
Hucaru <7339035+Hucaru@users.noreply.github.com>
2024-08-07 19:25:16 +0100

    joypad input handling properly working

--- src/joypad.cpp
+++ src/joypad.cpp
@@ -23,37 +23,66 @@ set_joypad_state(Input_events *events, Joypad *joypad)
     {
         test = RIGHT;
     }
+    else if (keyboard_up(events, Input_events::KEY_CODE::RIGHT))
+    {
+        joypad->state = 0xFF;
+    }
     else if (keyboard_down(events, Input_events::KEY_CODE::LEFT))
     {
         test = LEFT;
+    }
+    else if (keyboard_up(events, Input_events::KEY_CODE::LEFT))
+    {
+        joypad->state = 0xFF;
     }
     else if (keyboard_down(events, Input_events::KEY_CODE::UP))
     {
         test = UP;
     }
+    else if (keyboard_up(events, Input_events::KEY_CODE::UP))
+    {
+        joypad->state = 0xFF;
+    }
     else if (keyboard_down(events, Input_events::KEY_CODE::DOWN))
     {
         test = DOWN;
+    }
+    else if (keyboard_up(events, Input_events::KEY_CODE::DOWN))
+    {
+        joypad->state = 0xFF;
     }
     else if (keyboard_down(events, Input_events::KEY_CODE::A))
     {
         test = A;
     }
+    else if (keyboard_up(events, Input_events::KEY_CODE::A))
+    {
+        joypad->state = 0xFF;
+    }
     else if (keyboard_down(events, Input_events::KEY_CODE::B))
     {
         test = B;
+    }
+    else if (keyboard_up(events, Input_events::KEY_CODE::B))
+    {
+        joypad->state = 0xFF;
     }
     else if (keyboard_down(events, Input_events::KEY_CODE::BACK))
     {
         test = SELECT;
     }
+    else if (keyboard_up(events, Input_events::KEY_CODE::BACK))
+    {
+        joypad->state = 0xFF;
+    }
     else if (keyboard_down(events, Input_events::KEY_CODE::RETURN))
     {
         test = START;
     }
-
-    joypad->button = false;
-    joypad->direction = false;
+    else if (keyboard_up(events, Input_events::KEY_CODE::BACK))
+    {
+        joypad->state = 0xFF;
+    }
 
     // Check if previously the corresponding bit was set to 1
     u8 prev_state = joypad->state & test;
@@ -68,9 +97,9 @@ set_joypad_state(Input_events *events, Joypad *joypad)
         {
             joypad->direction = true;
         }
-    }
 
-    joypad->state = 0xFF & (~test);
+        joypad->state = 0xFF & (~test);
+    }
 }
 
 void
@@ -81,9 +110,11 @@ handle_input_event(Memory_Bus *memory_bus)
     if (memory_bus->joypad.button && (req & JOYPAD_BUTTON_REQUEST) == 0)
     {
         perform_interrupt(memory_bus, INTERRUPT_JOYPAD);
+        memory_bus->joypad.button = false;
     }
     else if (memory_bus->joypad.direction && (req & JOYPAD_DIRECTION_REQUEST) == 0)
     {
         perform_interrupt(memory_bus, INTERRUPT_JOYPAD);
+        memory_bus->joypad.direction = false;
     }
 }
